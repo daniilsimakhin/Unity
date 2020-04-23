7ce2caf5cc9fda6f441a6e1bd37062c9fc67be4d
daniilsimakhin <52642856+daniilsimakhin@users.noreply.github.com>
2020-04-23 13:24:10 +0700

    signalization

---
+++ Learning2/Assets/Scripts/Character.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private float _speed = 10;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {   
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {        
+            transform.Translate(Vector2.left * _speed * Time.deltaTime);
+            _spriteRenderer.flipX = true;
+            _animator.SetFloat("Speed", 2);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * _speed * Time.deltaTime);
+            _spriteRenderer.flipX = false;           
+            _animator.SetFloat("Speed", 2);
+        }
+        else
+        {
+            _animator.SetFloat("Speed", 0);
+        }
+    }
+}
---
+++ Learning2/Assets/Scripts/Door.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Transform _character;
+    private SpriteRenderer _colorChanger;
+    private AudioSource _audioClip;
+    private bool _inDoor = false;
+    private bool _inHome = false;
+    private Coroutine _light;
+
+    private void Start()
+    {
+        _colorChanger = GetComponent<SpriteRenderer>();
+        _audioClip = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (_inDoor && Input.GetKeyDown(KeyCode.E) && !_inHome)
+        {
+            _character.position = new Vector3(_character.position.x, _character.position.y, 0.5f);
+            gameObject.transform.Find("Walls").gameObject.SetActive(true);
+            _inHome = true;
+            StartCoroutine(_VolimeSound(_inHome));
+            _light = StartCoroutine(_FlashingLight(_inHome));
+        } 
+        else if (_inDoor &&  Input.GetKeyDown(KeyCode.E) && _inHome)
+        {           
+            _character.position = new Vector3(_character.position.x, _character.position.y, -1);
+            gameObject.transform.Find("Walls").gameObject.SetActive(false);
+            _inHome = false;
+            StopCoroutine(_light);
+            _colorChanger.color = Color.white;
+            StartCoroutine(_VolimeSound(_inHome));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _inDoor = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _inDoor = false;
+    }
+
+    private IEnumerator _VolimeSound(bool check)
+    {
+        if (check)
+        {
+            _audioClip.Play();
+            for (int i = 0; i <= 10; i++)
+            {
+                _audioClip.volume = i * 0.1f;
+                yield return new WaitForSeconds(0.25f);
+            }
+        }
+        else if (!check)
+        {
+            for (int i = 9; i >= 0; i--)
+            {
+                _audioClip.volume = i * 0.1f;
+                yield return new WaitForSeconds(0.25f);
+            }
+            _audioClip.Stop();
+        }
+    }
+
+    private IEnumerator _FlashingLight(bool check)
+    {
+        while (check)
+        {            
+            if (_colorChanger.color == Color.white)
+            {
+                _colorChanger.color = Color.red;
+            }
+            else if (_colorChanger.color == Color.red)
+            {
+                _colorChanger.color = Color.white;
+            }    
+            yield return new WaitForSeconds(0.2f);
+        }        
+    }
+}
