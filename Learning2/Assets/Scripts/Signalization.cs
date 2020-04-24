bbd3b2a02b91a8692bef943da23305ecc7724c50
daniilsimakhin <52642856+daniilsimakhin@users.noreply.github.com>
2020-04-24 17:28:57 +0700

    Исправление 2

--- Learning2/Assets/Scripts/Signalization.cs
+++ Learning2/Assets/Scripts/Signalization.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof (SpriteRenderer), typeof (AudioSource))]
 public class Signalization : MonoBehaviour
 {
-    [SerializeField] private Transform _character;
+    [SerializeField] private Transform _characterTransform;
     private SpriteRenderer _colorChanger;
     private AudioSource _audioClip;
     private Coroutine _light;
@@ -16,63 +16,50 @@ public class Signalization : MonoBehaviour
         _audioClip = GetComponent<AudioSource>();
     }
 
-    void Update()
+    private void Update()
     {
-        _IsDoorOpen(_isDoorOpen);
+        IsDoorOpen(_isDoorOpen);
     }
 
-    private void _IsDoorOpen(bool doorOpen)
+    private void IsDoorOpen(bool doorOpen)
     {
         if (doorOpen)
         {
-            if (Input.GetKeyDown(KeyCode.E) && _character.position.z == -1)
+            if (Input.GetKeyDown(KeyCode.E) && _characterTransform.position.z == -1)
             {
-                _PlayerEnter(0.5f, true);
-                StartCoroutine(_VolimeSound(0.5f, 5));
-                _light = StartCoroutine(_FlashingLight(0.5f));
+                PlayerEnter(0.5f, true);
+                StartCoroutine(VolimeSound(0.5f, 5));
+                _light = StartCoroutine(FlashingLight(0.5f));
             }
-            else if (Input.GetKeyDown(KeyCode.E) && _character.position.z == 0.5f)
+            else if (Input.GetKeyDown(KeyCode.E) && _characterTransform.position.z == 0.5f)
             {
-                _PlayerEnter(-1, false);
-                StartCoroutine(_VolimeSound(-1, 5));
+                PlayerEnter(-1, false);
+                StartCoroutine(VolimeSound(-1, 5));
                 StopCoroutine(_light);
                 _colorChanger.color = Color.white;
             }
         }
     }
 
-    private void _PlayerEnter(float changeLayer, bool walls)
+    private void PlayerEnter(float changeLayer, bool walls)
     {
-        _character.position = new Vector3(_character.position.x, _character.position.y, changeLayer);
+        _characterTransform.position = new Vector3(_characterTransform.position.x, _characterTransform.position.y, changeLayer);
         gameObject.transform.Find("Walls").gameObject.SetActive(walls);
     }
 
-
-    private IEnumerator _VolimeSound(float changeLayer, float durationChangeVolume)
+    private IEnumerator VolimeSound(float changeLayer, float durationChangeVolume)
     {
         _audioClip.Play();
-        for (int i = 0; i <= 10; i++)
+        for (int i = 1; i <= 10; i++)
         {
-            //if (changeLayer == 0.5f)
-            //{
-            //    _audioClip.volume = i * 0.1f;
-            //}
-            //else
-            //{
-            //    _audioClip.volume = (10 - i) * 0.1f;
-            //    if (_audioClip.volume == 0)
-            //    {
-            //        _audioClip.Stop();
-            //    }
-            //}
-            float j = changeLayer == 0.5f ? (_audioClip.volume = i * 0.1f) : ((_audioClip.volume = (10 - i) * 0.1f) == 0) ? 10: -10;
-            if (j == 10)
+            float audioVolume = changeLayer == 0.5f ? (_audioClip.volume = (0 + i) * 0.1f) : (_audioClip.volume = (10 - i) * 0.1f);                         
+            if (audioVolume == 0)
                 _audioClip.Stop();
             yield return new WaitForSeconds(1 / durationChangeVolume);
         }
     }
 
-    private IEnumerator _FlashingLight(float changeLayer)
+    private IEnumerator FlashingLight(float changeLayer)
     {
         while (changeLayer == 0.5f)
         {
@@ -84,7 +71,6 @@ public class Signalization : MonoBehaviour
             {
                 _colorChanger.color = Color.white;
             }
-            //var x = changeLayer == 0.5f ? (_colorChanger.color  == Color.white) ? (_colorChanger.color = Color.red) : (_colorChanger.color = Color.white);
             yield return new WaitForSeconds(0.1f);
         }
     }
